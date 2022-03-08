48cfdbba18a975e1a6de6f771288f96b30f5af2c
Delilah <ila0406@gmail.com>
2022-03-07 21:34:25 -0700

    added sudo code and when's

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const generateHTML = require('./utils/generateHTML');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// Emma Reimer (she/her)  1 day ago
+// Yes - my menu function asks:
+// Do you want to (with choices)
+// Add an engineer
+// Add an intern
+// Finish building team
+// And it gets called after every function
+// And then I have one more function renderHtml that gets called if they select finish building team
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -28,6 +37,44 @@ const questions = [
         name: 'managerOffice'
     }
 ];
+
+return inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'role',
+        message: "Please choose what you employee's role is",
+        choices: ['Engineer', 'Intern']
+    },
+    {
+        type: 'input',
+        name: 'name',
+        message: "What's is your employee's name?", 
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please enter the employee's ID.",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the employee's email.",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please enter your employee's github name.",
+        // adding a when field to make the prompt only visible when the role is an Engineer
+        when: (input) => input.role === "Engineer",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Please enter the intern's school",
+        // added code to make the school question only visible to the Intern role
+        when: (input) => input.role === "Intern",
+    },
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
