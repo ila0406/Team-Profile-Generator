9046f845dc1a5b98f2be00d427d20880d818840d
Delilah <ila0406@gmail.com>
2022-03-08 21:07:54 -0700

    updated inquirer questions & more html output

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // const generateMarkdown = require('./utils/generateMarkdown');
-const generateHTML = require('./dist/generateHTML');
+const generateHTML = require('./lib/generateHTML');
 
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
@@ -14,66 +14,48 @@ const fs = require('fs');
 // And it gets called after every function
 // And then I have one more function renderHtml that gets called if they select finish building team
 
-// TODO: Create an array of questions for user input
+// An array of questions for user input
 const questions = [
     {
-        type: 'input',
-        message: 'Enter team managers name:',
-        name: 'managerName'
+        type: 'list',
+        name: 'role',
+        message: 'Select type of employee',
+        choices: ['Manager','Engineer','Intern']
     },
     {
         type: 'input',
-        message: 'Enter managers employee ID:',
-        name: 'managerID'
+        message: 'Enter employees name:',
+        name: 'name'
     },
     {
         type: 'input',
-        message: 'Enter managers email address:',
-        name: 'mangerEmail'
+        message: 'Enter employees ID:',
+        name: 'id'
     },
     {
         type: 'input',
-        message: 'Enter managers office number:',
-        name: 'managerOffice'
+        message: 'Enter employees email address:',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'Enter office number:',
+        name: 'officeNumber',
+        when: (input) => input.role === "Manager",
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter engineer's github name:",
+        when: (input) => input.role === "Engineer",
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "Enter the intern's school:",
+        when: (input) => input.role === "Intern",
     }
 ];
-
-// return inquirer.prompt ([
-//     {
-//         type: 'list',
-//         name: 'role',
-//         message: "Please choose what you employee's role is",
-//         choices: ['Engineer', 'Intern']
-//     },
-//     {
-//         type: 'input',
-//         name: 'name',
-//         message: "What's is your employee's name?", 
-//     },
-//     {
-//         type: 'input',
-//         name: 'id',
-//         message: "Please enter the employee's ID.",
-//     },
-//     {
-//         type: 'input',
-//         name: 'email',
-//         message: "Please enter the employee's email.",
-//     },
-//     {
-//         type: 'input',
-//         name: 'github',
-//         message: "Please enter your employee's github name.",
-//         // adding a when field to make the prompt only visible when the role is an Engineer
-//         when: (input) => input.role === "Engineer",
-//     },
-//     {
-//         type: 'input',
-//         name: 'school',
-//         message: "Please enter the intern's school",
-//         // added code to make the school question only visible to the Intern role
-//         when: (input) => input.role === "Intern",
-//     },
 
 
 // TODO: Create a function to write README file
@@ -89,7 +71,7 @@ function init() {
     inquirer.prompt(questions)
         .then(answers => {
             console.log(answers)
-             writeToFile('./utils/index.html',answers);
+             writeToFile('./dist/index.html',answers);
             // writeToFile('../../Generated-Readme/README.md',answers);
         }
     );
