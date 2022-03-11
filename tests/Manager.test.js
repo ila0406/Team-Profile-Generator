190f0562d9805fce72284578c29a85111ed8f53e
Delilah <ila0406@gmail.com>
2022-03-10 18:44:33 -0700

    Used bootstrap for css

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -9,9 +9,13 @@ describe("Manager", () => {
             });
     });
 
-    // describe("getOfficeNumber", () => {
+    describe("getOfficeNumber", () => {
+        it('Gets office number of manager', () => {
+            const manager = new Manager('Delilah', '1', 'ila0406@gmail.com','123');
 
-    // });
+            expect(manager.officeNumber).toBeDefined();
+        });
+    });
 
     describe("getRole", () => {
         it('Gets role of manager', () => {
